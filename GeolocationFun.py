18755885527dbcf57905c494597cc3eb10657aa0
Tom Fish <guerillero.wikipedia@gmail.com>
2015-01-15 19:06:03 -0500

    forgot KD

--- GeolocationFun.py
+++ GeolocationFun.py
@@ -7,11 +7,14 @@ __licence__ = 'MIT'
 
 # Script starts here
 import arcpy
+from arcpy import env
+from arcpy.sa import *
 
 # Fix the file paths.
 points = "E:\Documents\GIS\Wikipedia\wiki.gdb\PointsWGS84"
 counties = "E:\Documents\GIS\Wikipedia\wiki.gdb\US_Counties"
 output = "E:\Documents\GIS\Wikipedia\wiki.gdb\countiesWithArticles"
+KDOut = "E:\Documents\GIS\Wikipedia\wiki.gdb\KD"
 
 # setup
 mxd = arcpy.mapping.MapDocument('CURRENT')
@@ -23,5 +26,9 @@ PolyLyr.name = "County Layer"
 if PntLyr.name == "PointsLayer":
     # I only want/need to see the points on earth. This should speed this up quite a bit
     PntLyr.definitionQuery = "'globe' = 'earth'"
+    # Do the join and save it
     arcpy.SpatialJoin_analysis(PolyLyr, PntLyr, output)
+    # Do the KD and then save it
+    KD = KernelDensity(PntLyr, "NONE")
+    KD.save(KDOut)
 
